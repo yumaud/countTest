4e32bdc012392b5a9df0a0ce9c27849e63bf2bf9
Yuma9819 <yuma.next.2018@outlook.jp>
2014-09-09 21:16:47 +0900

    test

--- counttest/ViewController.m
+++ counttest/ViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
     
     number = 0;
@@ -29,7 +28,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
